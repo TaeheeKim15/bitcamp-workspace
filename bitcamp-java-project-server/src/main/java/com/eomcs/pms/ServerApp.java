e529e962ea22e1fd6abcc06e45b2ed3d75065e86
TaeheeKim15 <utility_maker@naver.com>
2020-10-16 19:00:18 +0900

    .

--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/ServerApp.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/ServerApp.java
@@ -6,65 +6,124 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import com.eomcs.context.ApplicationContextListener;
+import com.eomcs.pms.handler.Command;
+import com.eomcs.pms.listener.AppInitListener;
+import com.eomcs.pms.listener.DataHandlerListener;
+import com.eomcs.pms.listener.RequestMappingListener;
 
-// stateful 통신
-// => 클라이언트가 연결되면 클라이언트가 보낸 메시지를 그대로 리턴해 준다
 public class ServerApp {
-  public static void main(String[] args) throws Exception {
-    try ( ServerSocket ss = new ServerSocket(8888)) {
-      System.out.println("서버 실행중");
 
-      new Thread(new ClientHandler(ss.accept())).start();
+  // 클라이언트가 "stop" 명령을 보내면 이 값이 true로 변경된다.
+  // - 이 값이 true 이면 다음 클라이언트 접속할 때 서버를 종료한다.
+  static boolean stop = false;
+
+  // 옵저버와 공유할 맵 객체
+  static Map<String,Object> context = new Hashtable<>();
+
+  // 옵저버를 보관할 컬렉션 객체
+  List<ApplicationContextListener> listeners = new ArrayList<>();
+
+  // 옵저버를 등록하는 메서드
+  public void addApplicationContextListener(ApplicationContextListener listener) {
+    listeners.add(listener);
+  }
+
+  // 옵저버를 제거하는 메서드
+  public void removeApplicationContextListener(ApplicationContextListener listener) {
+    listeners.remove(listener);
+  }
+
+  // 옵저버에게 통지한다.
+  private void notifyApplicationContextListenerOnServiceStarted() {
+    for (ApplicationContextListener listener : listeners) {
+      listener.contextInitialized(context);
+    }
+  }
+
+  // 옵저버에게 통지한다.
+  private void notifyApplicationContextListenerOnServiceStopped() {
+    for (ApplicationContextListener listener : listeners) {
+      listener.contextDestroyed(context);
+    }
+  }
+
+  public void service(int port) {
+
+    notifyApplicationContextListenerOnServiceStarted();
+
+    try (ServerSocket serverSocket = new ServerSocket(port)) {
+      System.out.println("서버 실행 중...");
+
+      while (true) {
+        Socket clientSocket = serverSocket.accept();
+
+        if (stop) {
+          break;
+        }
+        // 람다 문법 사용
+        new Thread(() -> handleClient(clientSocket)).start();
+      }
 
     } catch (Exception e) {
       e.printStackTrace();
     }
+
+    notifyApplicationContextListenerOnServiceStopped();
   }
 
-  // 클라이언트 요청을 처리하는 일을 한다
-  // 다른 클래스가 사용할 일이 없기 때문에 private 으로 선언한다
-  // 바깥 클래스의 특정 인스턴스 멤버를 사용할
-  private static class ClientHandler implements Runnable {
-    Socket socket;
+  public static void main(String[] args) {
+    ServerApp server = new ServerApp();
 
-    public ClientHandler(Socket socket) {
-      this.socket = socket;
-    }
+    // 리스너(옵저버/구독자) 등록
+    server.addApplicationContextListener(new AppInitListener());
+    server.addApplicationContextListener(new DataHandlerListener());
+    server.addApplicationContextListener(new RequestMappingListener());
 
-    @Override
-    public void run() {
+    server.service(8888);
+  }
 
-      InetAddress address = this.socket.getInetAddress();
-      System.out.printf("클라이언트(%s)가 연결되었습니다.\n",
-          address.getHostAddress());
+  private static void handleClient(Socket clientSocket) {
+    InetAddress address = clientSocket.getInetAddress();
+    System.out.printf("클라이언트(%s)가 연결되었습니다.\n",
+        address.getHostAddress());
 
-      try (Socket socket = this.socket; // try 블록을 떠날 때 close()가 자동 호출된다.
-          BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-          PrintWriter out = new PrintWriter(socket.getOutputStream())) {
-
-        while (true) {
-          String request = in.readLine();
-
-          sendResponse(out, request);
-
-          if (request.equalsIgnoreCase("quit"))
-            break;
-        }
+    try (Socket socket = clientSocket; // try 블록을 떠날 때 close()가 자동 호출된다.
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream())) {
 
 
-      } catch (Exception e) {
-        System.out.println("클라이언트와의 통신 오류!");
+      // 클라이언트가 보낸 요청을 읽는다.
+      String request = in.readLine();
+
+      if (request.equalsIgnoreCase("stop")) {
+        stop = true; // 서버의 상태를 멈추라는 의미로 true로 설정한다.
+        out.println("서버를 종료하는 중입니다...");
+        out.println();
+        out.flush();
+        return;
       }
 
-      System.out.printf("클라이언트(%s)와의 연결을 끊었습니다.\n",
-          address.getHostAddress());
+      Command command = (Command) context.get(request);
+      if (command != null) {
+        command.execute(out, in);
+      } else {
+        out.println("해당 명령을 처리할 수 없습니다!");
+      }
+
+      // 응답의 끝을 알리는 빈 문자열을 보낸다.
+      out.println();
+      out.flush();
+
+    } catch (Exception e) {
+      System.out.println("클라이언트와의 통신 오류!");
     }
 
-    private void sendResponse(PrintWriter out, String message) {
-      out.println(message);
-      out.println(); // 응답이 끝났음을 알리는 빈 줄을 보낸다.
-      out.flush();
-    }
+    System.out.printf("클라이언트(%s)와의 연결을 끊었습니다.\n",
+        address.getHostAddress());
   }
 }
-
