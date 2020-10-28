cff2b2071d77a0186e89134be2d8485401505b32
TaeheeKim15 <utility_maker@naver.com>
2020-10-28 21:01:25 +0900

    .

---
+++ bitcamp-java-basic/src/main/java/com/eomcs/jdbc/ex01/Exam0110.java
@@ -0,0 +1,23 @@
+package com.eomcs.jdbc.ex01;
+
+import java.sql.DriverManager;
+
+public class Exam0110 {
+  public static void main(String[] args) {
+
+
+    try {
+      java.sql.Driver driver = new org.mariadb.jdbc.Driver();
+      DriverManager.registerDriver(driver);
+
+      java.sql.Driver driver2 = DriverManager.getDriver("jdbc:mysql:");
+      System.out.println(driver2);
+
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+
+}
