047c3ce6b2e87d39a043c934627ce6e74ca1d02d
TaeheeKim15 <utility_maker@naver.com>
2020-11-23 20:58:18 +0900

    .

--- bitcamp-java-basic/src/main/java/com/eomcs/Hello2.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/Hello2.java
@@ -1,9 +1,11 @@
-class Hello2 {
-     public static void main(String[] args) {
-         System.out.println("com/eomcs//Hello2.java");
-         System.out.println("Hello, world!");
+package com.eomcs;
 
-     }
+class Hello2 {
+  public static void main(String[] args) {
+    System.out.println("com/eomcs//Hello2.java");
+    System.out.println("Hello, world!");
+
+  }
 }
 
 // 주제: 폴더를 구분하여 소스 파일 관리하기
